29b81cfc1b4df62a99ed036efe4b3578a01e0708
JasmineRolin <75620908+JasmineRolin@users.noreply.github.com>
2025-02-07 14:50:36 +0100

    remove test packafe

--- domain/src/Request.jl
+++ domain/src/Request.jl
@@ -1,18 +1,18 @@
 module Requests 
 
-using Dates, ..TimeWindows, ..Locations
+using ..TimeWindows, ..Locations
 
 export Request, Activity,CustomerType
 
 #==
  Enum that describes activity type
 ==#
-@enum Activity PICKUP=0 DELIVERY=1
+@enum RequestType PICKUP=0 DELIVERY=1
 
 #==
  Enum that describes customer type
 ==# 
-@enum CustomerType WALKING=0 WHEELCHAIR=1
+@enum MobilityType WALKING=0 WHEELCHAIR=1
 
 
 #== 
@@ -20,16 +20,14 @@ export Request, Activity,CustomerType
 ==#
 struct Request
     id::Int 
-    activity::Activity
-    customerType::CustomerType
-    date::Date # The date where the request happens
-    timeReceived::DateTime # The date and time the reqeust is received 
+    requestType::RequestType
+    mobilityType::MobilityType
+    callTime::Int # The time the reqeust is received (minutes after midnight)
     pickupTimeWindow::TimeWindow # Time window for pickup
     deliveryTimeWindow::TimeWindow # Time window for delivery 
     pickupLocation::Location # Pickup location
-    deliveryLocation::Location # Delivery location 
+    dropOffTimeWindow::Location # Delivery location 
     maximumRideTime::Int # Maximum ride time in minutes 
-    numberOfTravellers::Int # Number of travellers including customer 
 end
 
 
--- testpackage/src/testpackage.jl
+++
@@ -1,5 +0,0 @@
-module testpackage
-
-greet() = print("Hello World!")
-
-end # module testpackage
