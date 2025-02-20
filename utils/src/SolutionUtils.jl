1eab6dd2b025995bbe61f625b278a563e92f6c67
JasmineRolin <75620908+JasmineRolin@users.noreply.github.com>
2025-02-20 13:28:16 +0100

    update solutionsutils.jl

--- utils/src/SolutionUtils.jl
+++ utils/src/SolutionUtils.jl
@@ -5,15 +5,40 @@ using UnPack, ..RouteUtils
 #==
 # Function to check feasibility of solution 
 ==#
-function checkSolutionFeasibility(solution::Solution)
+function checkSolutionFeasibility(scenario::Scenario,solution::Solution)
     @unpack vehicleSchedules, totalCost, nTaxi, totalRideTime, totalDistance, idleTime = solution
 
-    # 
+    # Collect ids of all activities in scenario 
+    activityIds = vcat([req.pickUpActivity.id, req.dropOffActivity.id] for req in scenario.requests) 
+    nActivities = length(activityIds)
+
+    # Keep track of serviced activities 
+    servicedActivities = zeros(Bool,nActivities)
 
     # Check all routes 
     for vehicleSchedule in vehicleSchedules
         feasible, msg, servicedActivities = checkRouteFeasibility(vehicleSchedule)
+
+        # Return if route is not feasible 
+        if !feasible
+            return false, msg
+        end
+
+        # Update serviced activities
+        for activity in servicedActivities
+            if servicedActivities[activity]
+                msg = "SOLUTION INFEASIBLE: Activity $(activity) is serviced more than once"
+                return false, msg
+            end
+
+            servicedActivities[activity] = true
+        end
     end
+
+    # Check that all activities are serviced
+
+
+    return true 
     
 end
 
