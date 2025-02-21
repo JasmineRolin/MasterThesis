7eff0c622208a919f0fc613f557cd68c9d6da804
JasmineRolin <75620908+JasmineRolin@users.noreply.github.com>
2025-02-21 15:45:54 +0100

    create empty vehicle constructor

--- domain/src/Vehicle.jl
+++ domain/src/Vehicle.jl
@@ -12,7 +12,15 @@ struct Vehicle
     maximumRideTime::Int # Minutes 
     capacities::Dict 
     totalCapacity::Int
-    
+
+    # Constructors
+    function Vehicle()
+        return new(0, TimeWindow(0,0), 0, Location("",0,0), 0, Dict(), 0)
+    end
+
+    function Vehicle(id::Int, availableTimeWindow::TimeWindow, depotId::Int, depotLocation::Location, maximumRideTime::Int, capacities::Dict, totalCapacity::Int)
+        return new(id, availableTimeWindow, depotId, depotLocation, maximumRideTime, capacities, totalCapacity)
+    end
 end
 
 
