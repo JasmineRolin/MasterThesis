f025dc7172401b6b4b28c7355c1cc0b4bd9ae90b
JasmineRolin <75620908+JasmineRolin@users.noreply.github.com>
2025-02-27 08:59:44 +0100

    update remove method

--- alns/src/DestroyMethods.jl
+++ alns/src/DestroyMethods.jl
@@ -85,32 +85,75 @@ end
 #==
  Method to remove activity at idx from route
 ==#
-function removeRequestsFromSchedule!(time::Array{Int,Int},schedule::Vector{ActivityAssignment},requestsToRemove::Vector{Int})
+function removeRequestsFromSchedule!(time::Array{Int,Int},schedule::VehicleSchedule,requestsToRemove::Vector{Int})
 
     # Remove requests from schedule
     for requestsToRemove in requestsToRemove
         pickUpPosition,dropOffPosition = findPositionOfRequest(schedule,requestId)
 
         # Remove pickup activity 
-        # Extend waiting activity before pick up 
-        if schedule[pickUpPosition-1].activity.activityType == WAITING
             schedule[pickUpPosition-1].endOfServiceTime = schedule[pickUpPosition+1].startOfServiceTime - time[schedule[pickUpPosition-1].activity.id,schedule[pickUpPosition+1].activity.id]
-            # TODO: update KPIs
-            deleteat!(schedule,pickUpPosition)
-        # Extend waiting activity after pick up
-        elseif schedule[dropOffPosition+1].activity.activityType == WAITING
-            schedule[pickUpPosition+1].startOfServiceTime = schedule[pickUpPosition-1].startOfServiceTime + time[schedule[pickUpPosition-1].activity.id,schedule[pickUpPosition+1].activity.id]
-            # TODO: update KPIs
-            deleteat!(schedule,pickUpPosition)
-        # Insert waiting activity 
-        else
-            
-        end
+        removeActivityFromRoute(time,schedule,pickUpPosition,dropOffPosition)
 
         # Remove drop off activity 
-
+        removeActivityFromRoute(time,schedule,dropOffPosition-1,-1)
     end
 
+
+end
+
+#==
+ Method to remove activity from route 
+==#
+function removeActivityFromRoute(time::Array{Int,Int},distance::Array{Float64,Float64},schedule::VehicleSchedule,idx::Int)
+
+    # Retrieve activities before and after activity to remove
+    route = schedule.route
+    activityToRemove = route[idx]
+    activityAssignmentBefore = route[idx-1]
+    activityAssignmentAfter = route[idx+1]
+
+    # Remove activity 
+    distanceDelta = 0.0
+    timeDelta = 0
+    idleTimeDelta = 0
+    costDelta = 0.0
+    # Extend waiting activity before activity to remove
+    if activityAssignmentBefore.activityType == WAITING
+
+        # Update waiting activity 
+        oldIdleTime = activityAssignmentBefore.endOfServiceTime - activityAssignmentBefore.startOfServiceTime
+        activityAssignmentBefore.endOfServiceTime = activityAssignmentAfter.startOfServiceTime - time[activityAssignmentBefore.activity.id,activityAssignmentAfter.activity.id]
+
+        # Update deltas 
+        deltaDistance = distance[activityAssignmentBefore.activity.id,activityAssignmentAfter.activity.id] - distance[activityAssignmentBefore.activity.id,activityToRemove.activity.id] - distance[activityToRemove.activity.id,activityAssignmentAfter.activity.id]
+        deltaIdleTime = (activityAssignmentBefore.endOfServiceTime - activityAssignmentBefore.startOfServiceTime) - oldIdleTime
+
+        # Delete activity
+        deleteat!(route,idx)
+
+       
+    # Extend waiting activity after activity to remove
+    elseif activityAssignmentAfter.activity.activityType == WAITING
+        activityAssignmentAfter.startOfServiceTime = activityAssignmentBefore.startOfServiceTime + time[activityAssignmentBefore.activity.id,activityAssignmentAfter.activity.id]
+       
+        deleteat!(route,pickUpPosition)
+
+
+
+        # TODO: update KPIs
+
+    # Insert waiting activity before activity to remove
+    else
+        startOfWaitingActivity = activityAssignmentBefore.endOfServiceTime
+        endOfWaitingActivity = activityAssignmentBefore.startOfServiceTime - time[activityAssignmentBefore.activity.id,activityAssignmentAfter.activity.id]
+
+        waitingActivity = Activity(activityAssignmentBefore.id,-1,WAITING,WALKING,activityAssignmentBefore.location,TimeWindow(startOfWaitingActivity,endOfWaitingActivity))
+        waitingActivityAssignment = ActivityAssignment(waitingActivity,activityAssignmentBefore.vehicle,startOfWaitingActivity,endOfWaitingActivity)
+        
+        route[idx] = waitingActivityAssignment
+        # TODO: update KPIs
+    end
 
 end
 
