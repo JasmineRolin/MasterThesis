941bfa93a64ad64ce5e5f4cd51adc3c18b77edb5
JasmineRolin <75620908+JasmineRolin@users.noreply.github.com>
2025-03-28 14:12:57 +0100

    uncomment test

--- tests/SimulationFrameworkTest.jl
+++ tests/SimulationFrameworkTest.jl
@@ -6,21 +6,21 @@ using simulationframework
 # #==
 # # Test SimulationFrameworkUtils
 # ==#
-# @testset "test SimulationFrameworkUtils" begin 
-#     requestFile = "tests/resources/RequestsToTestSimulation.csv"
-#     vehiclesFile = "tests/resources/Vehicles.csv"
-#     parametersFile = "tests/resources/Parameters.csv"
-#     distanceMatrixFile = "tests/resources/distanceMatrix_SmallToTestSimulation.txt"
-#     timeMatrixFile = "tests/resources/timeMatrix_SmallToTestSimulation.txt"
-#     scenarioName = "SmallToTestSimulation."
+@testset "test SimulationFrameworkUtils" begin 
+    requestFile = "tests/resources/RequestsToTestSimulation.csv"
+    vehiclesFile = "tests/resources/Vehicles.csv"
+    parametersFile = "tests/resources/Parameters.csv"
+    distanceMatrixFile = "tests/resources/distanceMatrix_SmallToTestSimulation.txt"
+    timeMatrixFile = "tests/resources/timeMatrix_SmallToTestSimulation.txt"
+    scenarioName = "SmallToTestSimulation."
 
-#     # Read instance 
-#     scenario = readInstance(requestFile,vehiclesFile,parametersFile,scenarioName,distanceMatrixFile,timeMatrixFile)
+    # Read instance 
+    scenario = readInstance(requestFile,vehiclesFile,parametersFile,scenarioName,distanceMatrixFile,timeMatrixFile)
 
-#     # Simulate scenario 
-#     solution = simulateScenario(scenario)
+    # Simulate scenario 
+    solution = simulateScenario(scenario)
 
-#     feasible, msg = checkSolutionFeasibility(scenario,solution,scenario.offlineRequests)
-#     @test feasible == true
-#     @test msg == ""
-# end
+    feasible, msg = checkSolutionFeasibility(scenario,solution,scenario.offlineRequests)
+    @test feasible == true
+    @test msg == ""
+end
