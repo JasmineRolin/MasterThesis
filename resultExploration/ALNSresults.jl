b90ff87f180d0d307150358c710077a9519f33c3
Astrid Liv Vesterbrandt <astrid@vesterbrandt.dk>
2025-05-15 13:44:59 +0200

    Started, not really done anything

---
+++ resultExploration/ALNSresults.jl
@@ -0,0 +1,62 @@
+using Test 
+using utils 
+using simulationframework
+using onlinesolution
+using domain
+using CSV
+
+
+
+
+
+
+function main(n::Int, nExpectedPercentage::Float64, gamma::Float64, date::String, run::String, resultType::String, i::Int)
+
+    vehiclesFile = string("Data/Konsentra/",n,"/Vehicles_",n,"_",gamma,".csv")
+    parametersFile = "tests/resources/Parameters.csv"
+    alnsParameters = "tests/resources/ALNSParameters_offline.json"
+    outPutFolder = string("resultExploration/results/",date,"/",run,"/",resultType,"/",n)
+    outputFiles = Vector{String}()
+    gridFile = string("Data/Konsentra/grid.json")
+
+    #for i in 1:10
+
+        ## ------------------- Dynamic case ---------------- 
+        requestFile = string("Data/Konsentra/",n,"/GeneratedRequests_",n,"_",i,".csv")
+        distanceMatrixFile = string("Data/Matrices/",n,"/GeneratedRequests_",n,"_",i,"_distance.txt")
+        timeMatrixFile =  string("Data/Matrices/",n,"/GeneratedRequests_",n,"_",i,"_time.txt")
+        scenarioName = string("Gen_Data_",n,"_",i)
+        push!(outputFiles, outPutFolder*"/Simulation_KPI_"*string(scenarioName)*".json")
+
+        # Read scenario 
+        #TODO use pre calculated distance and time matrix file. 
+        scenario = readInstance(requestFile,vehiclesFile,parametersFile,scenarioName,"","",gridFile)
+        solution, requestBank = simulateScenario(scenario,requestFile,distanceMatrixFile,timeMatrixFile,vehiclesFile,parametersFile,alnsParameters,scenarioName,anticipation = false,printResults = false, saveResults = true,gridFile = gridFile, outPutFileFolder = outPutFolder)
+
+        # TODO remove when stable
+        state = State(solution,scenario.onlineRequests[end],0)
+        feasible, msg = checkSolutionFeasibilityOnline(scenario,state)
+        @test feasible == true
+        @test msg == ""
+
+
+        ## ------------------- All is known ----------------
+        # TODO insert offline solution used on all requests when other reuslt is merged in
+    #end
+    dfResults = processResults(outputFiles)
+    CSV.write(outPutFolder*"/results.csv", dfResults)
+
+end
+
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    n = parse(Int, ARGS[1])
+    nExpectedPercentage = parse(Float64, ARGS[2])
+    gamma = parse(Float64, ARGS[3])
+    date = ARGS[4]
+    run = ARGS[5]
+    resultType = ARGS[6]
+    dataset = parse(Int, ARGS[7])
+    main(n, nExpectedPercentage, gamma, date, run, resultType, dataset)
+end
