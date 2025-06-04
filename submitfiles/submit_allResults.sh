fd252a3e06c04bc4112615ea5ea576046c12c524
Astrid <astrid@vesterbrandt.dk>
2025-06-04 10:50:41 +0200

    result all result new original data

--- submitfiles/submit_allResults.sh
+++ submitfiles/submit_allResults.sh
@@ -6,14 +6,14 @@
 n_requests_list=("20" "100" "300" "500")
 anticipation_levels=("0.4")
 run_tags=("run1" "run2" "run3" "run4" "run5")
-gamma="0.5"
-date="2025-06-03_original_0.5"
+gamma="0.7"
+date="2025-06-03_original_0.7"
 ####################
 
 mkdir -p submitfiles/generated_jobs
 
 # Define case types
-case_types=("InHindsight")
+case_types=("InHindsight" "BaseCase" "AnticipationKeepExpected")
 
 for case_type in "${case_types[@]}"; do
   for n_requests in "${n_requests_list[@]}"; do
