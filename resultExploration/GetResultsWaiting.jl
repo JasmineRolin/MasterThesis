debe6b135066c6fb127a2bd3481de0e941da9a09
Jasmine Rolin <jasmine@live.dk>
2025-06-15 14:58:03 +0200

    in hindsight dynamic

--- resultExploration/GetResultsWaiting.jl
+++ resultExploration/GetResultsWaiting.jl
@@ -3,7 +3,7 @@ using CSV, DataFrames, Statistics, Plots, Plots.PlotMeasures, PrettyTables, JSON
 
 nRequestList = [20,100,300,500]
 nRuns = 5
-relocateVehiclesList = [("true","true"),("true","false"),("false","false")]#,("inhindsight","")]
+relocateVehiclesList = [("true","true"),("true","false"),("false","false"),("inhindsight","")]
 gamma = 0.7
 baseFolder = "runfiles/output/Waiting/Dynamic/"
 plotName = "Dynamic"
